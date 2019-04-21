1c6115e4633e4cef17b94b829a6c7b5cec7a241f
Anton Trublin <anton.trublin@gmail.com>
2019-04-20 21:02:11 -0700

    Make header component responsive

--- src/components/Page/Header/index.js
+++ src/components/Page/Header/index.js
@@ -1,41 +1,28 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import { Container, Menu, Button } from 'semantic-ui-react';
+import { Container, Menu, Dropdown } from 'semantic-ui-react';
 import { GoogleLogin } from 'react-google-login';
 import { AuthConsumer } from '../../../contexts/auth';
+import './styles.scss';
 
 const AppHeader = () => (
   <AuthConsumer>
     {({ user, isFetching, login, logout }) => {
       return (
-        <Menu fixed="top" inverted color="violet" size="large">
-          <Container
-            style={{
-              minHeight: '72px',
-            }}
-          >
+        <Menu fixed="top" inverted color="violet" size="large" id="header">
+          <Container id="container">
             <Menu.Item header>
               <Link to="/">Stream Viewer</Link>
             </Menu.Item>
 
             {user ? (
-              <Menu.Item position="right">
-                <div
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-evenly',
-                    minWidth: '200px',
-                  }}
-                >
-                  <strong>{user.nickname}</strong>
-                  <div>
-                    <Button inverted type="button" onClick={logout}>
-                      Log Out
-                    </Button>
-                  </div>
-                </div>
-              </Menu.Item>
+              <Menu.Menu position="right">
+                <Dropdown item text={user.nickname}>
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Menu.Menu>
             ) : (
               <Menu.Item position="right">
                 <GoogleLogin
@@ -44,11 +31,6 @@ const AppHeader = () => (
                   onSuccess={login}
                   onFailure={() => {}}
                   cookiePolicy="single_host_origin"
-                  style={{
-                    height: '10px',
-                    maxHeight: '10px',
-                    background: 'green',
-                  }}
                   disabled={isFetching}
                 />
               </Menu.Item>
